6fad6270035eb0daeb3f9d6132dbffdd38a30e6b
sundharesanraman725 <141895274+sundharesanraman725@users.noreply.github.com>
2023-09-12 09:51:23 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,34 @@
+
+public class forLoop1 {
+
+	public static void main(String[] args) {
+		int i;
+		i = 1;
+		System.out.println("Value of variable i is " + i);
+		
+		i = i+1;
+		System.out.println("Value of variable i is " + i);
+		
+		i = i+1;
+		System.out.println("Value of variable i is " + i);
+		
+		i = i+1;
+		System.out.println("Value of variable i is " + i);
+		
+		i = i+1;
+		System.out.println("Value of variable i is " + i);
+		
+		i = i+1;
+		System.out.println("Value of variable i is " + i);
+		
+		
+		for (int j = 1; j <501; j = j + 1) {
+			System.out.println("!!!!!!!!!!!!");
+			System.out.println("Value of j is " + j);
+			System.out.println("!!!!!!!!!!!!");
+		}
+		
+
+	}
+
+}
